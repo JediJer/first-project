8023609e70b6c213e5ea2a9ef6f9eae1c53b9aeb
JediJer <biggardo@gmail.com>
2011-02-03 09:32:20 -0700

    Revert "version 2"
    This reverts commit 72ae4986f1a08b88b25c64d78f1608333e9c1456.

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,7 +7,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		System.out.println("Goodbye World")
 	}
 
 }
